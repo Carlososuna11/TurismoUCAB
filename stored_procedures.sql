ea1a677259cec488cb0bc19a08a82c4c649d1a41
Carlos Osuna <carlosalvaroosuna1@gmail.com>
2021-12-09 21:46:29 -0400

    Fix insert

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -10,8 +10,7 @@ IS
     V_blob BLOB;
 BEGIN
     DBMS_OUTPUT.enable;
-    INSERT INTO pais(id_pais, nombre, foto, nacionalidad)
-    VALUES (id, nombre, EMPTY_BLOB(), nacionalidad) RETURNING foto INTO V_blob;
+    INSERT INTO PAIS VALUES (id, nombre, EMPTY_BLOB(), nacionalidad) RETURNING foto INTO V_blob;
     V_bfile := BFILENAME('ORACLECLRDIR', foto);
     DBMS_LOB.OPEN(V_bfile, DBMS_LOB.LOB_READONLY);
     DBMS_LOB.LOADFROMFILE(V_blob, V_bfile, SYS.DBMS_LOB.GETLENGTH(V_bfile));
