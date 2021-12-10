49d886645d5714ab7f2ed7f78ebeaac1f5425b6d
Carlos Osuna <carlosalvaroosuna1@gmail.com>
2021-12-10 19:37:48 -0400

    fix insert

--- insert.sql
+++ insert.sql
@@ -247,228 +247,228 @@ VALUES (SYSDATE,round(DBMS_RANDOM.VALUE (0, 1)),15);
 
 -- INICIO DESTINOS ------------------------
 BEGIN
-    INSERTAR_DESTINO(
-    id_destino.nextVal,
-    'JuanGriego',
-    'Paraiso idilico y hermoso, es la Bahia de Juan Griego; colmada de aguas mansas y sin oleaje con un colorido en tonalidades que van desde el azul celeste hasta los mas intensos y adornada por pelicanos que posan sobre los botes de los pescadores de la region.',
-    'juangriego.jpeg',
-    'Margarita.mp4'
-    );
+    -- INSERTAR_DESTINO(
+    -- id_destino.nextVal,
+    -- 'JuanGriego',
+    -- 'Paraiso idilico y hermoso, es la Bahia de Juan Griego; colmada de aguas mansas y sin oleaje con un colorido en tonalidades que van desde el azul celeste hasta los mas intensos y adornada por pelicanos que posan sobre los botes de los pescadores de la region.',
+    -- 'juangriego.jpeg',
+    -- 'Margarita.mp4'
+    -- );
 
-    INSERTAR_DESTINO(
-    id_destino.nextVal,
-    'Pampatar',
-    'La ciudad de Pampatar es la capital del municipio Maneiro, en la isla de Margarita, estado Nueva Esparta (Venezuela). Su antiguo nombre era Pampatare o Mampatar. Pampatar es voz indígena que significa «casa de sal».',
-    'pampatar.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(
+    -- id_destino.nextVal,
+    -- 'Pampatar',
+    -- 'La ciudad de Pampatar es la capital del municipio Maneiro, en la isla de Margarita, estado Nueva Esparta (Venezuela). Su antiguo nombre era Pampatare o Mampatar. Pampatar es voz indígena que significa «casa de sal».',
+    -- 'pampatar.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(
-    id_destino.nextVal,
-    'Porlamar',
-    'Porlamar, es una ciudad ubicada al sureste de la Isla de Margarita, Nueva Esparta, Venezuela; capital del municipio Mariño, es el principal centro urbano y económico de la isla.',
-    'porlamar.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(
+    -- id_destino.nextVal,
+    -- 'Porlamar',
+    -- 'Porlamar, es una ciudad ubicada al sureste de la Isla de Margarita, Nueva Esparta, Venezuela; capital del municipio Mariño, es el principal centro urbano y económico de la isla.',
+    -- 'porlamar.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(
-    id_destino.nextVal,
-    'Isla de Coche',
-    'La Isla de Coche es una isla ubicada en el estado Nueva Esparta, Venezuela, en la costa occidental de la República Bolivariana de Venezuela. Su nombre original era Coche, y fue cambiado en el año 2000 por la Isla de Margarita, debido a que la isla de Coche se encuentra en el lugar donde se encuentra la capital del municipio Mariño.',
-    'coche.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(
+    -- id_destino.nextVal,
+    -- 'Isla de Coche',
+    -- 'La Isla de Coche es una isla ubicada en el estado Nueva Esparta, Venezuela, en la costa occidental de la República Bolivariana de Venezuela. Su nombre original era Coche, y fue cambiado en el año 2000 por la Isla de Margarita, debido a que la isla de Coche se encuentra en el lugar donde se encuentra la capital del municipio Mariño.',
+    -- 'coche.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(
-    id_destino.nextVal,
-    'Los Roques',
-    'El Archipielago de Los Roques ubicado en el mar Caribe contiene más de 300 islas y cayos que rodean una laguna de 400 km cuadrados.',
-    'los_roques.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(
+    -- id_destino.nextVal,
+    -- 'Los Roques',
+    -- 'El Archipielago de Los Roques ubicado en el mar Caribe contiene más de 300 islas y cayos que rodean una laguna de 400 km cuadrados.',
+    -- 'los_roques.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(
-    id_destino.nextVal,
-    'Isla La Tortuga ',
-    'La Tortuga es una isla en el Mar Caribe conformada por una isla mayor y varios islotes y cayos más pequeños.',
-    'tortuga.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(
+    -- id_destino.nextVal,
+    -- 'Isla La Tortuga ',
+    -- 'La Tortuga es una isla en el Mar Caribe conformada por una isla mayor y varios islotes y cayos más pequeños.',
+    -- 'tortuga.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(
-    id_destino.nextVal,
-    'Morrocoy',
-    'El Parque Nacional Morrocoy es una isla protegida y una reserva marina en la costa caribeña de Venezuela. Se encuentra en el estado Falcón y es conocida por sus manglares, sus playas y sus cayos, como el Cayo Sombrero.',
-    'morrocoy.jpg',
-    'Margarita.mp4'
-    );
+    -- INSERTAR_DESTINO(
+    -- id_destino.nextVal,
+    -- 'Morrocoy',
+    -- 'El Parque Nacional Morrocoy es una isla protegida y una reserva marina en la costa caribeña de Venezuela. Se encuentra en el estado Falcón y es conocida por sus manglares, sus playas y sus cayos, como el Cayo Sombrero.',
+    -- 'morrocoy.jpg',
+    -- 'Margarita.mp4'
+    -- );
 
-    INSERTAR_DESTINO(
-    id_destino.nextVal,
-    'Morrocoy',
-    'El Parque Nacional Morrocoy es una isla protegida y una reserva marina en la costa caribeña de Venezuela. Se encuentra en el estado Falcón y es conocida por sus manglares, sus playas y sus cayos, como el Cayo Sombrero.',
-    'morrocoy.jpg',
-    'Margarita.mp4'
-    );
+    -- INSERTAR_DESTINO(
+    -- id_destino.nextVal,
+    -- 'Morrocoy',
+    -- 'El Parque Nacional Morrocoy es una isla protegida y una reserva marina en la costa caribeña de Venezuela. Se encuentra en el estado Falcón y es conocida por sus manglares, sus playas y sus cayos, como el Cayo Sombrero.',
+    -- 'morrocoy.jpg',
+    -- 'Margarita.mp4'
+    -- );
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Bahia de Cata','La bahía de Cata es una playa del estado Aragua, de gran extensión, finísima arena, aguas cristalinas y adornadas por la sombra de erguidos cocoteros.',
-    'bahia_cata.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Bahia de Cata','La bahía de Cata es una playa del estado Aragua, de gran extensión, finísima arena, aguas cristalinas y adornadas por la sombra de erguidos cocoteros.',
+    -- 'bahia_cata.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Choroní','Choroní tiene varias playas hermosas, Playa Grande, Tipire, El Diario, Valle seco, Chuao y varios lugaros para visitar con lancha',
-    'choroni.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Choroní','Choroní tiene varias playas hermosas, Playa Grande, Tipire, El Diario, Valle seco, Chuao y varios lugaros para visitar con lancha',
+    -- 'choroni.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Lecheria','Lecheria tiene varias playas hermosas y varios lugares para visitar.',
-    'lecheria.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Lecheria','Lecheria tiene varias playas hermosas y varios lugares para visitar.',
+    -- 'lecheria.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Mochima','Mochima es uno de los destinos turísticos más espectaculares de Venezuela. Playa Colorada es mundialmente famosa por su fina arena de color dorado, casi rojizo, que da nombre al lugar.',
-    'mochima.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Mochima','Mochima es uno de los destinos turísticos más espectaculares de Venezuela. Playa Colorada es mundialmente famosa por su fina arena de color dorado, casi rojizo, que da nombre al lugar.',
+    -- 'mochima.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Cuyagua','Cuyagua es un pueblo ubicado en la costa de Aragua, Venezuela. Entre sus ríos se encuentran el Pozo de Arena y el Pozo San Pedro. Tiene una playa que posee un fuerte oleaje donde se practica el surf y el bodyboard',
-    'cuyagua.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Cuyagua','Cuyagua es un pueblo ubicado en la costa de Aragua, Venezuela. Entre sus ríos se encuentran el Pozo de Arena y el Pozo San Pedro. Tiene una playa que posee un fuerte oleaje donde se practica el surf y el bodyboard',
+    -- 'cuyagua.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Cayo Sombrero','Cayo Sombrero​ es el nombre de una isla del mar Caribe que pertenece al parque nacional Morrocoy​.',
-    'cayo_sombrero.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Cayo Sombrero','Cayo Sombrero​ es el nombre de una isla del mar Caribe que pertenece al parque nacional Morrocoy​.',
+    -- 'cayo_sombrero.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Urama','Urama es un pueblo en el estado Vargas, tiene varias playas hermosas y rios para visitar.',
-    'urama.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Urama','Urama es un pueblo en el estado Vargas, tiene varias playas hermosas y rios para visitar.',
+    -- 'urama.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Los Frailes','Uno de los lugares menos visitados en Margarita son las islas de Los Frailes. Son esas islas que se ven a lo lejos desde las playas de Guacuco, El Tirano, Parguito o El Agua.',
-    'los_frailes.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Los Frailes','Uno de los lugares menos visitados en Margarita son las islas de Los Frailes. Son esas islas que se ven a lo lejos desde las playas de Guacuco, El Tirano, Parguito o El Agua.',
+    -- 'los_frailes.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Los Testigos','El Archipiélago Los Testigos es de clima cálido, semi-árido y con un tiempo suave y agradable durante la mayoría del año, un clima muy parecido al vecino Archipiélago de Los Frailes.',
-    'los_testigos.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Los Testigos','El Archipiélago Los Testigos es de clima cálido, semi-árido y con un tiempo suave y agradable durante la mayoría del año, un clima muy parecido al vecino Archipiélago de Los Frailes.',
+    -- 'los_testigos.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'La Orchila','Isla la Orchila es una isla en la costa de Venezuela. Tiene numerosas playas, incluida una donde la arena es notablemente rosada.',
-    'la_orchila.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'La Orchila','Isla la Orchila es una isla en la costa de Venezuela. Tiene numerosas playas, incluida una donde la arena es notablemente rosada.',
+    -- 'la_orchila.jpg',
+    -- 'Margarita.mp4');
 
-    INSERTAR_DESTINO(id_destino.nextVal,
-    'Las Aves','Es una pequeña y remota isla venezolana, lugar de descanso y reproducción para las aves marinas y para la tortuga verde.',
-    'las_aves.jpg',
-    'Margarita.mp4');
+    -- INSERTAR_DESTINO(id_destino.nextVal,
+    -- 'Las Aves','Es una pequeña y remota isla venezolana, lugar de descanso y reproducción para las aves marinas y para la tortuga verde.',
+    -- 'las_aves.jpg',
+    -- 'Margarita.mp4');
 
 -- FIN DESTINOS ------------------------
 
 -- INICIO PROVEEDORES ------------------------
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Hesperia',
-        'Hesperia_logo.png',
-        'Hesperia_hotel.jpg'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Hesperia',
+    --     'Hesperia_logo.png',
+    --     'Hesperia_hotel.jpg'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Unik Hotel',
-        'unik_logo.png',
-        'unik_hotel.jpg'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Unik Hotel',
+    --     'unik_logo.png',
+    --     'unik_hotel.jpg'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Margarita Dynasty Hotel & Suites',
-        'dynasty_logo.png',
-        'dynasty_hotel.jpg'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Margarita Dynasty Hotel & Suites',
+    --     'dynasty_logo.png',
+    --     'dynasty_hotel.jpg'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Proxima Ruta Venezuela',
-        'proxima_ruta_logo.png',
-        'proxima_ruta_feed.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Proxima Ruta Venezuela',
+    --     'proxima_ruta_logo.png',
+    --     'proxima_ruta_feed.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Mochileros Venezuela',
-        'mochileros_venezuela.jpg',
-        'mochileros_venezuela_feed.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Mochileros Venezuela',
+    --     'mochileros_venezuela.jpg',
+    --     'mochileros_venezuela_feed.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Ruta Extrema',
-        'ruta_extrema.png',
-        'ruta_extrema_feed.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Ruta Extrema',
+    --     'ruta_extrema.png',
+    --     'ruta_extrema_feed.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Fundación Azul Ambientalistas',
-        'fundacion_azul.jpg',
-        'fundacion_azul_foto.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Fundación Azul Ambientalistas',
+    --     'fundacion_azul.jpg',
+    --     'fundacion_azul_foto.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Fundación Tierra Viva',
-        'fundacion_tierra_viva.jpg',
-        'tierra_viva_foto.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Fundación Tierra Viva',
+    --     'fundacion_tierra_viva.jpg',
+    --     'tierra_viva_foto.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Recreación Bululu',
-        'bululu_logo.png',
-        'bululu_foto.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Recreación Bululu',
+    --     'bululu_logo.png',
+    --     'bululu_foto.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Sendero cojedes',
-        'sendero_cojedes_logo.jpg',
-        'sendero_cojedes_feed.jpg'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Sendero cojedes',
+    --     'sendero_cojedes_logo.jpg',
+    --     'sendero_cojedes_feed.jpg'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Rutas Mochileras',
-        'rutas_mochileras_logo.png',
-        'rutas_mochileras_feed.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Rutas Mochileras',
+    --     'rutas_mochileras_logo.png',
+    --     'rutas_mochileras_feed.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Suites Cristina',
-        'suites_cristina.png',
-        'suites_cristina_hotel.jpg'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Suites Cristina',
+    --     'suites_cristina.png',
+    --     'suites_cristina_hotel.jpg'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Woi-Kaa',
-        'woikaa_logo.jpg',
-        'woikaa_feed.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Woi-Kaa',
+    --     'woikaa_logo.jpg',
+    --     'woikaa_feed.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Marriot',
-        'marriot_logo.png',
-        'marriot_hotel.png'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Marriot',
+    --     'marriot_logo.png',
+    --     'marriot_hotel.png'
+    -- );
 
-    INSERTAR_PROVEEDOR(
-        id_proveedor.nextVal,
-        'Eurobuilding Express',
-        'eurobuilding_logo.png',
-        'eurobuilding_hotel.jpg'
-    );
+    -- INSERTAR_PROVEEDOR(
+    --     id_proveedor.nextVal,
+    --     'Eurobuilding Express',
+    --     'eurobuilding_logo.png',
+    --     'eurobuilding_hotel.jpg'
+    -- );
 
 -- FIN PROVEEDORES ------------------------
 
