072503e2306c29f766fba47518f943e58d8b6bcf
Carlos Osuna <carlosalvaroosuna1@gmail.com>
2021-12-11 14:20:14 -0400

    Add drop and fixes

--- modulo_inicio/modulo_inicio.sql
+++ modulo_inicio/modulo_inicio.sql
@@ -8,31 +8,36 @@ END;
 /
 CREATE OR REPLACE PACKAGE BODY MODULO_INICIO AS
     PROCEDURE ASIGNAR_PAISES IS
+        CURSOR cli IS 
+            SELECT * FROM CLIENTE;
+        cli_aux CLIENTE%ROWTYPE;
         id_pais_random NUMBER;
-        nombre_pais_random VARCHAR2;
+        nombre_pais_random VARCHAR2(100);
     BEGIN
-        FOR cli IN (
-            SELECT * from CLIENTE
-        )
-        LOOP
-            IF aceptar_o_rechazar(0.3) THEN
-                dbms_output.put_line('Asignando pais Venezuela al Cliente '||cli.datos.nombre||' '||cli.datos.apellido);
-                UPDATE CLIENTE 
-                SET pais_id = 10 
-                WHERE id_cliente=cli.id_cliente;
-            ELSE
-                SELECT id_pais,nombre 
-                INTO id_pais_random,nombre_pais_random 
-                FROM PAIS 
-                WHERE id_pais != 10 
-                ORDER BY DBMS_RANDOM.RANDOM ASC
-                FETCH FIRST 1 ROWS ONLY;
-                dbms_output.put_line('Asignando pais '|| nombre_pais_random ||' al Cliente '||cli.datos.nombre||' '||cli.datos.apellido);
-                UPDATE CLIENTE 
-                SET pais_id = id_pais_random
-                WHERE id_cliente=cli.id_cliente;
-            END IF; 
-        END LOOP;
+        OPEN cli;
+        FETCH cli into cli_aux;
+        WHILE cli%FOUND
+            LOOP
+                IF aceptar_o_rechazar(0.3) THEN
+                    dbms_output.put_line('Asignando pais Venezuela al Cliente '||cli_aux.datos.nombre||' '||cli_aux.datos.apellido);
+                    UPDATE CLIENTE 
+                    SET pais_id = 10 
+                    WHERE id_cliente=cli_aux.id_cliente;
+                ELSE
+                    SELECT id_pais,nombre 
+                    INTO id_pais_random,nombre_pais_random 
+                    FROM PAIS 
+                    WHERE id_pais != 10 
+                    ORDER BY DBMS_RANDOM.RANDOM ASC
+                    FETCH FIRST 1 ROWS ONLY;
+                    dbms_output.put_line('Asignando pais '|| nombre_pais_random ||' al Cliente '||cli_aux.datos.nombre||' '||cli_aux.datos.apellido);
+                    UPDATE CLIENTE 
+                    SET pais_id = id_pais_random
+                    WHERE id_cliente=cli_aux.id_cliente;
+                END IF; 
+                FETCH cli into cli_aux;
+            END LOOP;
+        CLOSE cli;
     END;
 
     -- PROCEDURE GENERAR_ALIANZAS IS
