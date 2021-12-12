f6b15b82a0dab7e08993e3a8ff639e85ef8f77a5
Carlos Osuna <carlosalvaroosuna1@gmail.com>
2021-12-11 23:01:32 -0400

    Fix Report 1

--- Reportes/stored_procedures_reportes.sql
+++ Reportes/stored_procedures_reportes.sql
@@ -1,11 +1,12 @@
 CREATE OR REPLACE PROCEDURE REPORTE_1 (cursorMemoria OUT SYS_REFCURSOR)
 AS
 BEGIN
-    OPEN cursorMemoria FOR SELECT MIN(disp.fecha_creacion) "Fecha de Creacion",
+    OPEN cursorMemoria FOR 
+    SELECT MIN(disp.fecha_creacion) "Fecha de Creacion",
     MIN(disp.fecha.fechaInicio) "Fecha desde",
     MAX(disp.fecha.fechaFin) "Fecha hasta",
     dest.nombre "Destino",
-    LISTAGG(DISTINCT '- '|| serv.nombre ,chr(13) || chr(10) ) "Servicios Ofrecidos" WITHIN GROUP (ORDER BY serv.nombre)
+    LISTAGG(DISTINCT '- '|| serv.nombre ,chr(13) || chr(10) )  WITHIN GROUP (ORDER BY serv.nombre) "Servicios Ofrecidos"
     FROM SERVICIO serv
     INNER JOIN DISPONIBILIDAD disp
     ON disp.id_servicio = serv.id_servicio
