68804b02a67178207ccb370e3160a51515e3b8d3
Carlos Osuna <carlosalvaroosuna1@gmail.com>
2021-12-11 23:03:09 -0400

    Fix Report 1

--- Reportes/stored_procedures_reportes.sql
+++ Reportes/stored_procedures_reportes.sql
@@ -5,12 +5,12 @@ BEGIN
     SELECT MIN(disp.fecha_creacion) "Fecha de Creacion",
     MIN(disp.fecha.fechaInicio) "Fecha desde",
     MAX(disp.fecha.fechaFin) "Fecha hasta",
-    dest.nombre "Destino",
+    LISTAGG(DISTINCT dest.nombre,'') "Destino",
     LISTAGG(DISTINCT '- '|| serv.nombre ,chr(13) || chr(10) )  WITHIN GROUP (ORDER BY serv.nombre) "Servicios Ofrecidos"
     FROM SERVICIO serv
     INNER JOIN DISPONIBILIDAD disp
     ON disp.id_servicio = serv.id_servicio
     INNER JOIN DESTINO dest
     ON dest.id_destino = serv.destino_id
-    GROUP BY serv.id_destino;
+    GROUP BY serv.destino_id;
 END;
