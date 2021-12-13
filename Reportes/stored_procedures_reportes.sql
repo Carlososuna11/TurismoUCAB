f19c405be459dc9026ff52eab73b0206df9f0993
Carlos Osuna <carlosalvaroosuna1@gmail.com>
2021-12-12 21:42:48 -0400

    Fix a F*cking ; expected

--- Reportes/stored_procedures_reportes.sql
+++ Reportes/stored_procedures_reportes.sql
@@ -55,28 +55,28 @@ begin
     paq.fechas.fechaInicio "Fecha Desde",
     paq.fechas.fechaFin "Fecha Hasta",
     dest.foto "Foto",
-    aux.caracteristicas "Caracteristicas",
+    -- aux.caracteristicas "Caracteristicas",
     '$ '||paq.precio|| ' por persona' "Costo"
     FROM DESTINO dest
     INNER JOIN PAQUETE paq
     ON paq.destino_id = dest.id_destino
-    INNER JOIN (
-        SELECT
-        dest.id_destino,
-        paq.id_paquete,
-        LISTAGG(DISTINCT '- '|| serv.nombre ,chr(13) || chr(10) )  WITHIN GROUP (ORDER BY serv.nombre) as caracteristicas
-        FROM DESTINO dest
-        INNER JOIN PAQUETE paq
-        ON paq.destino_id = dest.id_destino
-        INNER JOIN SUBSCRIPCION sub
-        ON sub.paquete_id = paq.id_paquete
-        INNER JOIN DISPONIBILIDAD disp
-        ON disp.id_disponibilidad = sub.disponibilidad_id
-        INNER JOIN SERVICIO serv
-        ON serv.id_servicio = disp.id_servicio
-        GROUP BY dest.id_destino, paq.id_paquete 
-    ) aux
-    ON aux.id_paquete = paq.id_paquete
+    -- INNER JOIN (
+    --     SELECT
+    --     dest.id_destino,
+    --     paq.id_paquete,
+    --     LISTAGG(DISTINCT '- '|| serv.nombre ,chr(13) || chr(10) )  WITHIN GROUP (ORDER BY serv.nombre) as caracteristicas
+    --     FROM DESTINO dest
+    --     INNER JOIN PAQUETE paq
+    --     ON paq.destino_id = dest.id_destino
+    --     INNER JOIN SUBSCRIPCION sub
+    --     ON sub.paquete_id = paq.id_paquete
+    --     INNER JOIN DISPONIBILIDAD disp
+    --     ON disp.id_disponibilidad = sub.disponibilidad_id
+    --     INNER JOIN SERVICIO serv
+    --     ON serv.id_servicio = disp.id_servicio
+    --     GROUP BY dest.id_destino, paq.id_paquete 
+    -- ) aux
+    -- ON aux.id_paquete = paq.id_paquete
     WHERE (paq.fechas.fechaInicio >= fechaInicio OR fechaInicio IS NULL) AND 
     ( paq.fechas.fechaFin <= fechaFin OR fechaFin IS NULL) AND
     ( dest.nombre = destino_nombre OR destino_nombre IS NULL) AND
